130b065f51c6119bfa1a28a7aac198bcd9cc04c1
luarocks <luarocks@pm.me>
2020-12-06 23:30:52 +0200

    Add extra types to luarocks.

---
+++ rocks/build-example.sh
@@ -0,0 +1,7 @@
+#!/bin/fish
+
+# alias lr="luarocks --lua-version=5.1 --local"
+# cd rocks
+# lr build typedobject-#.#-#.rockspec
+# lr pack typedobject-#.#-#.rockspec
+# lr upload typedobject-#.#-#.rockspec --api-key <your API key>
