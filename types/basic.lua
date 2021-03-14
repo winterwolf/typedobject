65350e6b4f23eaebcecaa46a45cdb0e2bb1ff96e
luarocks <luarocks@pm.me>
2021-03-14 07:54:17 +0200

    Better types

--- types/basic.lua
+++ types/basic.lua
@@ -2,14 +2,18 @@
 -- * "integer": integer number.
 -- * "float":   float number.
 -- * "char":    single utf8 string character.
+
+math.type = math.type or function(x)
+  if type(x) == "number" then
+    if string.find(x, "%.") then return "float"
+    else return "integer" end
+  end
+  return nil
+end
+
 return function(var, expected, got)
   if got == "number" then
-    if expected == "float" then
-      if string.find(var, "%.") then return true end
-    end
-    if expected == "integer" then
-      if not string.find(var, "%.") then return true end
-    end
+    if expected == math.type(var) then return true end
   elseif got == "string" then
     if expected == "char" then
       local utf8 = utf8 or require "utf8"
